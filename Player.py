f97b909e6ce43ec46cdf9de0603ea8178eea70ec
Rushil Arun <s-rarun@lwsd.org>
2023-11-09 13:40:46 -0800

    Finished test branch side scrolling implementation

--- Player.py
+++ Player.py
@@ -1,15 +1,30 @@
 import pygame
 
 class Player:
-    def __init__(self, x, y):
+    def __init__(self, x, y, character_size, width, bg_width, speed):
         self.character_pos_x = x
         self.character_pos_y = y
+        self.size = character_size
+        self.view_width = width
+        self.background_width = bg_width
+        self.box_viewpoint_x = 0
+        self.speed = speed
+
+    
     def move(self, pressed):
         if pressed[pygame.K_UP]:
-            self.character_pos_y -= 10
+            self.character_pos_y -= self.speed
         if pressed[pygame.K_DOWN]:
-            self.character_pos_y += 10
-        if pressed[pygame.K_RIGHT]:
-            self.character_pos_x += 10
-        if pressed[pygame.K_LEFT]:
-            self.character_pos_x -= 10
+            self.character_pos_y += self.speed
+        if pressed[pygame.K_RIGHT] and self.character_pos_x <= self.view_width - self.size:
+            if self.character_pos_x < (self.view_width / 2) or -self.box_viewpoint_x >= (self.background_width - self.view_width): 
+                self.character_pos_x += self.speed
+            else:
+                self.box_viewpoint_x -= self.speed
+        if pressed[pygame.K_LEFT] and self.character_pos_x > 0:
+            if self.character_pos_x > (self.view_width / 2) or self.box_viewpoint_x >= 0:
+                self.character_pos_x -= self.speed
+            else:
+                self.box_viewpoint_x += self.speed
+
+        return self.box_viewpoint_x
--- main.py
+++ main.py
@@ -8,9 +8,15 @@ window_length = 16 * size
 window_height = 12 * size
 screen = pygame.display.set_mode((window_length, window_height))
 background = pygame.Surface((window_length, window_height))
+
+character_size = 15
 black = (0, 0, 0)
 image = pygame.image.load('Castlevania_Test_Background.jpg')
-player = Player(100, 300)
+background_height = image.get_height()
+background_width = image.get_width()
+print(background_width)
+movement_speed = 5
+player = Player(100, 300, character_size, window_length, background_width, movement_speed)
 grid = Grid()
 print(grid.grid_return())
 
@@ -19,22 +25,24 @@ print(grid.grid_return())
 def game():
     character_pos_x = 100
     character_pos_y = 100
-    character_size = 60
+    
     image_x = 0
     image_y = 0
     game_over = False
     clock = pygame.time.Clock()
+    print(background_height, background_width)
     FPS = 60
     while not game_over:
         pressed = pygame.key.get_pressed()
-        player.move(pressed)
+        viewpoint_x = player.move(pressed)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 game_over = True
             
     
-        screen.blit(image, (image_x, image_y))
+        screen.blit(image, (viewpoint_x, 0))
+        
         pygame.draw.rect(screen, (20, 210, 99), (player.character_pos_x, player.character_pos_y, character_size, character_size))
         pygame.display.update()
         clock.tick(FPS)
