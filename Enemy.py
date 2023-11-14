9841e69222d9a452b41f0b926d8e3f6ad63541cb
Rushil Arun <s-rarun@lwsd.org>
2023-11-14 13:43:26 -0800

    Connecting sprite to enemy class

--- Enemy.py
+++ Enemy.py
@@ -2,10 +2,11 @@ import pygame
 
 class Enemy:
     def __init__(self):
-        self.image = pygame.image.load('sprite_can.png')
-        self.mask = pygame.mask.from_surface(self.image)
+        self.image = pygame.image.load('Enemy.png')
+        self.big_image = pygame.transform.scale(self.image, (100, 160))
+        self.mask = pygame.mask.from_surface(self.big_image)
         self.x = 100
         self.y = 100
     
     def draw(self, screen):
-        screen.blit(self.image, (100, 100))
+        screen.blit(self.big_image, (100, 100))
