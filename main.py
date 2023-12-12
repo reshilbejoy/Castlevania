f4313312e2d160a0b1bdd8c39c7c7f11a96ae1bb
Arun-Rushil <s-rarun@lwsd.org>
2023-12-12 13:42:27 -0800

    Changed the background to Background-easy.png

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ background = pygame.Surface((window_length, window_height))
 character_size = 50
 black = (0, 0, 0)
 image = pygame.image.load('Castlevania_Test_Background.jpg')
-ground = pygame.image.load('Background-easy.png')
-ground_height = ground.get_height()
-ground_width = ground.get_width()
+ground_image = pygame.image.load('Background-easy.png')
+ground_image = pygame.transform.scale(ground_image, (1600, 1200))
+ground_height = ground_image.get_height()
+ground_width = ground_image.get_width()
 print(ground_height, ground_width)
 background_height = image.get_height()
 background_width = image.get_width()
-print(background_width)
+
+
 
 
 player = Player(character_size, window_length, background_width, movement_speed, window_height + score_box, int(ground_height / 5), window_height, score_box)
@@ -35,7 +37,7 @@ grid = Grid()
 enemy = Enemy(40, window_length, window_height, player.isBgMoving, movement_speed, window_height + score_box - int(ground_height / 5))
 #print(grid.grid_return())
 platform = Platform(100, 10, window_length / 2, (window_height) - 50)
-ground = Platform(background_width, int(ground_height / 5), 0, window_height + score_box - int(ground_height / 5))
+ground = Platform(background_width, int(630 / 5), 0, window_height + score_box - int(630 / 5))
 
 #def offset(player, enemy):
     #return int(enemy.enemy_x - player.character_pos_x), int(enemy.enemy_y - player.character_pos_y)
@@ -64,7 +66,7 @@ def game():
         enemy.move(window_length)
         platform.move(player.box_viewpoint_x)
     
-        screen.blit(image, (viewpoint_x, score_box))
+        screen.blit(ground_image, (viewpoint_x, score_box))
         enemy.draw(screen)
         
         player.draw(screen)
@@ -78,7 +80,7 @@ def game():
             
         #if overlap != None and overlap[1] == platform.y:
             #print(overlap)
-        print((enemy.enemy_x - viewpoint_x) % 5)
+        #print((enemy.enemy_x - viewpoint_x) % 5)
         #if player.mask.overlap(enemy.mask, offset(player, enemy)):
         if player.rect.colliderect(enemy.rect):
             enemy.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
