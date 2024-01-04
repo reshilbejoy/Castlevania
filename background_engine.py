f97397b01db7f000f8e6be31cb7cf8396975e34e
Arun-Rushil <s-rarun@lwsd.org>
2024-01-04 12:35:58 -0800

    Starting to load in background

--- background_engine.py
+++ background_engine.py
@@ -11,10 +11,11 @@ pygame.init()
 size = 35
 height_ratio = 14
 length_ratio = 22
-window_size = (600,500)
-window = pygame.display.set_mode((length_ratio * size, height_ratio * size))
+window_size = (length_ratio * size, height_ratio * size)
+window = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Castlevania")
 _normal_background = pygame.transform.scale(pygame.image.load('Assets/Background-easy.png'), (1200, 1600))
+window.blit(_normal_background, (0, 0))
 _background_arr:List[pygame.Surface] = []
 _timer = pygame.time.Clock() 
 _draw_frame_size:Tuple[float,float] = (500,600)  #Width, Height
