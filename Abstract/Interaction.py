e1cc1b5b30ea1800a73d3b0b3c2be6adcdfe459b
Arun-Rushil <s-rarun@lwsd.org>
2024-01-16 13:45:15 -0800

    Fixing a typo

--- Abstract/Interaction.py
+++ Abstract/Interaction.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import List
 import pygame
 from abc import abstractmethod,ABC
-from Abstract.dynamic_sprite import DyanmicSprite
+from Abstract.dynamic_sprite import DynamicSprite
 from Abstract.Sprite import Sprite
 from Utils.signals import DamageMessage, InventoryMessage
 
@@ -22,10 +22,10 @@ class Interactable(Sprite,ABC):
     def life_span(self):
         pass
     
-    def damage_interaction(self,obj:DyanmicSprite,msg:DamageMessage):
+    def damage_interaction(self,obj:DynamicSprite,msg:DamageMessage):
         obj.handle_damage_interaction(msg)
     
-    def inventory_interaction(self,obj:DyanmicSprite,msg:InventoryMessage):
+    def inventory_interaction(self,obj:DynamicSprite,msg:InventoryMessage):
         obj.handle_inventory_interaction(msg)
 
     def update(self):
