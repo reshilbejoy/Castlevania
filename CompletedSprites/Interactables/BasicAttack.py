772975fac5e665937d3607a47ec435d44c35d717
Arun-Rushil <s-rarun@lwsd.org>
2024-02-02 17:04:02 -0800

    Increased attack speed a little

--- CompletedSprites/Interactables/BasicAttack.py
+++ CompletedSprites/Interactables/BasicAttack.py
@@ -6,7 +6,7 @@ from Utils.signals import DamageMessage, InventoryMessage, Item, TargetType
 from background_engine import BackgroundEngine
 
 
-attack_span_ms = 250
+attack_span_ms = 350
 
 class BasicAttack(Interactable):
     def __init__(self,hitbox,pose_supplier:Callable,damage_target:TargetType, remove_obj:Callable):
